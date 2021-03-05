d368cc94e03fe31240038adb1113ec88bf24afd1
JerryChen <pitaya943@icloud.com>
2021-03-06 02:33:50 +0800

    Initial Commit

---
+++ MyAppForOwner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAppForOwner
+//
+//  Created by 阿揆 on 2021/3/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAppForOwner/MyAppForOwnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAppForOwnerApp.swift
+//  MyAppForOwner
+//
+//  Created by 阿揆 on 2021/3/6.
+//
+
+import SwiftUI
+
+@main
+struct MyAppForOwnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
